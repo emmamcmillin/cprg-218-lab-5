d0b863a18e289b5197d4ba1e2a8b8fd8e5d9814f
emmamcmillin <48865335+emmamcmillin@users.noreply.github.com>
2024-04-03 21:33:48 -0600

    adding all the lab 5 files

---
+++ scripts.js
@@ -0,0 +1,50 @@
+
+  /* GET https://newsapi.org/v2/everything?q=Apple&from=2024-04-02&sortBy=popularity&apiKey=76fe5af7bd694c1abdbc3f641eb9c676 */
+
+
+const searchFrom = document.querySelector('.search');
+const input = document.querySelector('.input');
+const newsList = document.querySelector('.news-list');
+
+searchFrom.addEventListener('submit', retrieve)
+
+function retrieve(e){
+
+  if (input.value == ''){
+    alert('Input field is empty!')
+    return
+  }
+
+  newsList.innerHTML = ''
+
+  e.preventDefault()
+
+  const apiKey = '76fe5af7bd694c1abdbc3f641eb9c676'
+  let topic = input.value;
+  let url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`
+  
+
+  fetch(url).then((res)=>{
+    return res.json()
+  }).then((data)=>{
+    console.log(data)
+    data.articles.forEach(article =>{
+      let li = document.createElement('li');
+      let a = document.createElement('a');
+      let img = document.createElement('img'); // Create img element
+      a.setAttribute('href', article.url);
+      a.setAttribute('target', '_blank');
+      a.textContent = article.title;
+      img.src = article.urlToImage; // Set src attribute of img to article's image URL
+      img.alt = article.title; // Set alt attribute of img to article's title
+      li.appendChild(img); // Append img to li
+      li.appendChild(a);
+      newsList.appendChild(li);
+    })
+  }).catch((error) => {
+    console.log(error)
+  })
+}
+
+
+
